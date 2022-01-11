ddb0f756f58a9bdff4cd308c960613d9aad313cd
Chris Millar <cmillar@adobe.com>
2022-01-11 11:24:28 -0700

    Remove martech for the time being. Fix above the fold

--- blocks/service-status/service-status.js
+++
@@ -1,8 +0,0 @@
-import { loadScript } from '../../scripts/scripts.js';
-import createTag from '../../utils/tag.js';
-
-export default async function init(el) {
-  await loadScript('https://unpkg.com/@statuspage/status-widget/dist/index.js');
-  const status = createTag('statuspage-widget', { src: 'https://status.project-helix.io', appearance: 'badge' });
-  el.append(status);
-}
