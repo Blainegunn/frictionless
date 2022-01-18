46e052be795b5e2f8f507a0dced339d62a24e79d
Blaine Gunn <gunn@adobe.com>
2022-01-18 12:00:54 -0700

    adding martect back

--- blocks/mar-tech/mar-tech.js
+++ blocks/mar-tech/mar-tech.js
@@ -1,11 +1,11 @@
-// const params = new URLSearchParams(document.location.search)
-// const timeOutLength = parseInt(params.get('timeoutMT')) || 0;
+const params = new URLSearchParams(document.location.search)
+const timeOutLength = parseInt(params.get('timeoutMT')) || 0;
 
-// export default function init(el) {
-//     setTimeout(() => {
-//         const MT_URL = document.createElement('script');
-//         MT_URL.setAttribute('src','https://www.adobe.com/marketingtech/main.stage.min.js');
-//         document.getElementsByTagName('head')[0].appendChild(MT_URL);
-//         console.log('Marketing Tech Loaded');
-//     }, timeOutLength);
-// }
+export default function init(el) {
+    setTimeout(() => {
+        const MT_URL = document.createElement('script');
+        MT_URL.setAttribute('src','https://www.adobe.com/marketingtech/main.stage.min.js');
+        document.getElementsByTagName('head')[0].appendChild(MT_URL);
+        console.log('Marketing Tech Loaded');
+    }, timeOutLength);
+}
