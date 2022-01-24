a63e6958db059b7c5ef08a29b654be4a503c0dd9
Blaine Gunn <gunn@adobe.com>
2022-01-24 07:50:36 -0700

    martech added to html with optimized package

--- blocks/mar-tech/mar-tech.js
+++ blocks/mar-tech/mar-tech.js
@@ -3,9 +3,9 @@ const timeOutLength = parseInt(params.get('timeoutMT')) || 0;
 
 export default function init(el) {
     setTimeout(() => {
-        const MT_URL = document.createElement('script');
-        MT_URL.setAttribute('src','https://www.adobe.com/marketingtech/main.stage.min.js');
-        document.getElementsByTagName('head')[0].appendChild(MT_URL);
+        // const MT_URL = document.createElement('script');
+        // MT_URL.setAttribute('src','https://www.adobe.com/marketingtech/main.stage.min.js');
+        // document.getElementsByTagName('head')[0].appendChild(MT_URL);
         console.log('Marketing Tech Loaded');
     }, timeOutLength);
 }
